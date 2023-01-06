0dbb1cec5c4025ebf56a4c9bc4f8c6c3b69e5fef
nn-firc <2319221757@qq.com>
2023-01-06 21:37:06 +0800

    fix linux support

--- src/game/server/ai_movesolver.cpp
+++ src/game/server/ai_movesolver.cpp
@@ -15,13 +15,11 @@
 
 //-----------------------------------------------------------------------------
 
-#if !defined(PS3) && (!defined(_MSC_VER) || _MSC_VER < 1800)
-// This C99 function exists in VS 2013's math.h and for PS3 but are not currently available elsewhere.
-inline float round( float f )
+/*inline float round( float f )
 {
 	return (float)( (int)( f + 0.5 ) );
 }
-#endif
+#endif*/
 
 //-----------------------------------------------------------------------------
 // CAI_MoveSolver
